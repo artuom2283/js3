ee6f64f6a72f2aacd45e81f7397c7762006e80d3
Artem Puzenko <123868996+artuom2283@users.noreply.github.com>
2023-12-04 21:24:37 +0200

    Update date.js

--- date.js
+++ date.js
@@ -1,22 +1,3 @@
-// const now = new Date()
-
-const start = new Date(1000 * 60 * 60 * 24 * 365)
-
-const date = new Date(2011, 0, 4, 12, 42, 12)
-
-// console.log(now.getFullYear())
-// console.log(now.getMonth() + 1)
-// console.log(now.getDate())
-// console.log(now.getMinutes())
-// console.log(now.getSeconds())
-// console.log(now.getMilliseconds())
-
-// now.setFullYear(2055)
-
-// console.log(now.toDateString())
-// console.log(now.toTimeString())
-// console.log(now.toLocaleDateString())
-// console.log(now.toLocaleTimeString())
 
 // ======
 
